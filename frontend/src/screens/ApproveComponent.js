0668e3bfb1d0843a7412fead414b27086e49ffd3
Tom Nash <contacttomnash@gmail.com>
2018-08-05 11:49:23 +1000

    pls

--- frontend/src/screens/ApproveComponent.js
+++ frontend/src/screens/ApproveComponent.js
@@ -26,14 +26,18 @@ export default class ApproveScreen extends React.Component {
     var pictures_to_approve = [
       {
         title: 'Take a photo at the Gold Coast of coastal erosion',
-        description: 'The photo should be taken from the beach near Sturt Street.',
-        image_url: 'https://www.sbs.com.au/yourlanguage/sites/sbs.com.au.yourlanguage/files/styles/full/public/podcasts//site_197_Filipino_441839.JPG?itok=_kkKIULx',
+        description:
+          'The photo should be taken from the beach near Sturt Street.',
+        image_url:
+          'https://www.sbs.com.au/yourlanguage/sites/sbs.com.au.yourlanguage/files/styles/full/public/podcasts//site_197_Filipino_441839.JPG?itok=_kkKIULx',
         id: 1
       },
       {
         title: 'Check Mitchelton Court for storm damage.',
-        description: 'After the storm last night, check the street for any obstructions of the road.',
-        image_url: 'http://www.trbimg.com/img-57fa3a26/turbine/dp-hurricane-matthew-storm-damage-20161009',
+        description:
+          'After the storm last night, check the street for any obstructions of the road.',
+        image_url:
+          'http://www.trbimg.com/img-57fa3a26/turbine/dp-hurricane-matthew-storm-damage-20161009',
         id: 2
       }
     ]
@@ -45,11 +49,9 @@ export default class ApproveScreen extends React.Component {
             <Panel.Title componentClass="h3">{x.title}</Panel.Title>
           </Panel.Heading>
           <Panel.Body>
-            <img style={{width: '50%', height: '50%'}}src={x.image_url} />
+            <img style={{ width: '50%', height: '50%' }} src={x.image_url} />
           </Panel.Body>
-          <Panel.Body>
-            {x.description}
-          </Panel.Body>
+          <Panel.Body>{x.description}</Panel.Body>
           <Panel.Body>
             <ButtonToolbar>
               <Button bsStyle="success">Approve</Button>
@@ -65,17 +67,9 @@ export default class ApproveScreen extends React.Component {
         <Navbar inverse>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#home">Quest</a>
+              <a href="#home">Geohash</a>
             </Navbar.Brand>
           </Navbar.Header>
-          <Nav>
-            <NavItem eventKey={1} href="#">
-              To Approve
-            </NavItem>
-            <NavItem eventKey={2} href="#">
-              Approved
-            </NavItem>
-          </Nav>
         </Navbar>
         <Grid>
           <PanelGroup>{panels}</PanelGroup>
